286ab3a5a3e740480afee1558f7cd93b90af78b8
Omirkhon <161600612+Omirkhon@users.noreply.github.com>
2024-10-26 23:08:45 +0500

    Update sql-countries.sql

--- sql-countries.sql
+++ sql-countries.sql
@@ -39,3 +39,39 @@ select sum(population) as total_german_population from cities where country_id =
 select sum(c.population) as total_european_population from cities as c
 join countries as co on co.id = c.country_id
 where co.continent_id = 1;
+
+-- 23
+select c.name , sum(ci.population) as total_population
+from countries c
+join cities ci on c.id = ci.country_id
+group by c.name
+order by total_population desc;
+
+-- 24
+select name, max(population) as max
+from cities
+group by name
+order by max desc
+limit 1;
+
+-- 25
+select c.name , avg(ci.population) as avg_population
+from countries c
+join cities ci on c.id = ci.country_id
+group by c.name
+having sum(ci.population) > 1000000;
+
+-- 26
+select distinct c.name, co.name
+from countries c
+join cities ci on c.id = ci.country_id
+join continents co on c.continent_id = co.id
+where ci.population < 1000000;
+
+-- 27
+select co.name, sum(ci.population) as total_population
+from continents co
+join countries c on co.id = c.continent_id
+join cities ci on c.id = ci.country_id
+group by co.name
+order by total_population;
